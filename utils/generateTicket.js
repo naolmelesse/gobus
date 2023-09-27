a18bd8f3bd4e95ee8062bc8d231d8593aca0704f
Naol Melesse <naolmelesse91@gmail.com>
2023-09-27 12:09:54 +0300

    ticket generation bug

--- utils/generateTicket.js
+++ utils/generateTicket.js
@@ -1,26 +1,32 @@
 import axios from "axios";
 import dayjs from "dayjs";
 
-export function GenerateTicket(data){
+export async function GenerateTicket(data){
     // console.log(dayjs(data.date).format("hh:mmA ddd-DD-MMM"));
-    axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/tickets`, 
-    {
-        "data" :{
-            traveler_name: data.name,
-            traveler_email: data.email,
-            date: dayjs(data.date).format("MM/DD/YYYY HH:mm"),
-            ticket_id: generateTicketID(data.date, data.seats),
-            departure: data.from,
-            destination: data.to,
-            seat_number: data.seats,
-            slug: generateTicketID(data.date, data.seats),
-        }
-    }).then((res) => {
-        console.log(res);
-        // return res.data;
-    }).catch(err => {
-        console.log("error occurred: ", err.message);
-    })
+    try{
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/tickets`, 
+        {
+            "data" :{
+                traveler_name: data.name,
+                traveler_email: data.email,
+                date: dayjs(data.date).format("MM/DD/YYYY HH:mm"),
+                ticket_id: generateTicketID(data.date, data.seats),
+                departure: data.from,
+                destination: data.to,
+                seat_number: data.seats,
+                slug: generateTicketID(data.date, data.seats),
+            }
+        })
+        console.log(response);
+    }catch(err){
+            console.log("error occurred: ", err.message);
+    }
+    // .then((res) => {
+    //     console.log(res);
+    //     // return res.data;
+    // }).catch(err => {
+    //     console.log("error occurred: ", err.message);
+    // })
 }
 
 function generateTicketID(date, seats){
